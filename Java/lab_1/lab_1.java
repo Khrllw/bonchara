6f64b39bb41e332203567df1a34781cf9e8bc3fb
ALYoNka <khrllw@gmail.com>
2024-11-04 19:17:48 +0300

    new

---
+++ Java/lab_1/lab_1.java
@@ -0,0 +1,12 @@
+package lab_1;
+
+public class lab_1 {
+    public static void main(String[] args) {
+        MarketModel model = new MarketModel();
+        System.out.print("-------------- \033[1;31mMARKET MODEL\033[0m --------------\n");
+        if (!model.inputValues()){return;}
+        model.searchBalanceQuantity();
+        model.printValues();
+    }
+}
+
